20a6bd4e9ed213b06051346d9de6086f3683fc24
hummingengineer <hummingengineer@gmail.com>
2020-10-15 16:35:55 +0900

    feat: Import modules

--- 01 - Representative Selection Program/representative_selector.py
+++ 01 - Representative Selection Program/representative_selector.py
@@ -1,5 +1,8 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QToolTip
+from PyQt5.QtGui import QIcon, QPixmap, QFont
+from PyQt5.QtCore import QCoreApplication
+import random
 
 class RepresentativeSelector(QWidget):
     def __init__(self):
@@ -7,8 +10,27 @@ class RepresentativeSelector(QWidget):
         self.initialize_UI()
 
     def initialize_UI(self):
+        self.set_image()
+        self.set_button()
+        self.set_tooltip()
+        self.set_number()
+
+        self.setWindowTitle('Elect a representative!')
+        self.setWindowIcon(QIcon('img/cat.png'))
         self.setGeometry(500, 500, 400, 400)
         self.show()
+
+    def set_image(self):
+        pass
+        
+    def set_button(self):
+        pass
+
+    def set_tooltip(self):
+        pass
+
+    def set_number(self):
+        pass
 
 program_endless_loop = QApplication(sys.argv)
 exec_instance = RepresentativeSelector()
